31b25be4983ba35a3ab40e2764403f8cae88f7f6
be-Lim <dongpal0201@gmail.com>
2026-08-16 10:56:26 +0900

    Create src1.cpp

---
+++ Week12_Morphological_Image_Processing/src1.cpp
@@ -0,0 +1,240 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+// 2차원 배열 동적할당 위함
+unsigned char** imageMatrix;
+// 이진영상에서 
+unsigned char blankPixel = 255, imagePixel = 0;
+
+typedef struct {
+	int row, col;
+}pixel;
+
+int getBlackNeighbours(int row, int col) {
+
+	int i, j, sum = 0;
+
+	for (i = -1; i <= 1; i++) {
+		for (j = -1; j <= 1; j++) {
+			if (i != 0 || j != 0)
+				sum += (imageMatrix[row + i][col + j] == imagePixel);
+		}
+	}
+
+	return sum;
+}
+
+int getBWTransitions(int row, int col) {
+	return 	((imageMatrix[row - 1][col] == blankPixel && imageMatrix[row - 1][col + 1] == imagePixel)
+		+ (imageMatrix[row - 1][col + 1] == blankPixel && imageMatrix[row][col + 1] == imagePixel)
+		+ (imageMatrix[row][col + 1] == blankPixel && imageMatrix[row + 1][col + 1] == imagePixel)
+		+ (imageMatrix[row + 1][col + 1] == blankPixel && imageMatrix[row + 1][col] == imagePixel)
+		+ (imageMatrix[row + 1][col] == blankPixel && imageMatrix[row + 1][col - 1] == imagePixel)
+		+ (imageMatrix[row + 1][col - 1] == blankPixel && imageMatrix[row][col - 1] == imagePixel)
+		+ (imageMatrix[row][col - 1] == blankPixel && imageMatrix[row - 1][col - 1] == imagePixel)
+		+ (imageMatrix[row - 1][col - 1] == blankPixel && imageMatrix[row - 1][col] == imagePixel));
+}
+
+int zhangSuenTest1(int row, int col) {
+	int neighbours = getBlackNeighbours(row, col);
+
+	return ((neighbours >= 2 && neighbours <= 6)
+		&& (getBWTransitions(row, col) == 1)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel || imageMatrix[row + 1][col] == blankPixel)
+		&& (imageMatrix[row][col + 1] == blankPixel || imageMatrix[row + 1][col] == blankPixel || imageMatrix[row][col - 1] == blankPixel));
+}
+
+int zhangSuenTest2(int row, int col) {
+	int neighbours = getBlackNeighbours(row, col);
+
+	return ((neighbours >= 2 && neighbours <= 6)
+		&& (getBWTransitions(row, col) == 1)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel || imageMatrix[row][col - 1] == blankPixel)
+		&& (imageMatrix[row - 1][col] == blankPixel || imageMatrix[row + 1][col] == blankPixel || imageMatrix[row][col + 1] == blankPixel));
+}
+
+void zhangSuen(unsigned char* image, unsigned char* output, int rows, int cols) {
+
+	int startRow = 1, startCol = 1, endRow, endCol, i, j, count, processed;
+
+	pixel* markers;
+
+	imageMatrix = (unsigned char**)malloc(rows * sizeof(unsigned char*));
+
+	for (i = 0; i < rows; i++) {
+		imageMatrix[i] = (unsigned char*)malloc((cols + 1) * sizeof(unsigned char));
+		for (int k = 0; k < cols; k++) imageMatrix[i][k] = image[i * cols + k];
+	}
+
+	endRow = rows - 2;
+	endCol = cols - 2;
+	do {
+		markers = (pixel*)malloc((endRow - startRow + 1) * (endCol - startCol + 1) * sizeof(pixel));
+		count = 0;
+
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
+				if (imageMatrix[i][j] == imagePixel && zhangSuenTest1(i, j) == 1) {
+					markers[count].row = i;
+					markers[count].col = j;
+					count++;
+				}
+			}
+		}
+
+		processed = (count > 0);
+
+		for (i = 0; i < count; i++) {
+			imageMatrix[markers[i].row][markers[i].col] = blankPixel;
+		}
+
+		free(markers);
+		markers = (pixel*)malloc((endRow - startRow + 1) * (endCol - startCol + 1) * sizeof(pixel));
+		count = 0;
+
+		for (i = startRow; i <= endRow; i++) {
+			for (j = startCol; j <= endCol; j++) {
+				if (imageMatrix[i][j] == imagePixel && zhangSuenTest2(i, j) == 1) {
+					markers[count].row = i;
+					markers[count].col = j;
+					count++;
+				}
+			}
+		}
+
+		if (processed == 0)
+			processed = (count > 0);
+
+		for (i = 0; i < count; i++) {
+			imageMatrix[markers[i].row][markers[i].col] = blankPixel;
+		}
+
+		free(markers);
+	} while (processed == 1);
+
+
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < cols; j++) {
+			output[i * cols + j] = imageMatrix[i][j];
+		}
+	}
+}
+
+void SaveBMPFile(BITMAPFILEHEADER hf, BITMAPINFOHEADER hInfo, RGBQUAD* hRGB, int W, int H, BYTE* Output, const char* FileName)
+{
+	FILE* fp = fopen(FileName, "wb");
+	if (hInfo.biBitCount == 24) {
+		fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+		fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+		fwrite(Output, sizeof(BYTE), W * H * 3, fp);
+	}
+	else {
+		fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp);
+		fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+		fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
+		fwrite(Output, sizeof(BYTE), W * H, fp);
+	}
+	fclose(fp);
+}
+
+void InverseImage(BYTE* Img, BYTE* Out, int W, int H)
+{
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++)
+	{
+		Out[i] = 255 - Img[i];
+	}
+}
+
+void Erosion(BYTE* Img, BYTE* Out, int W, int H) 
+{
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			if (Img[i * W + j] == 255)	//전경화소라면
+			{
+				if (!(Img[(i - 1) * W + j] == 255 &&
+					Img[(i + 1) * W + j] == 255 &&
+					Img[i * W + j - 1] == 255 &&
+					Img[i * W + j + 1] == 255))	//4주변 화소가 모두 전경화소가 아니라면
+					Out[i * W + j] = 0;
+				else
+					Out[i * W + j] = 255;
+			}
+			else
+				Out[i * W + j] = 0;
+		}
+	}
+}
+
+void Dilation(BYTE* Img, BYTE* Out, int W, int H)
+{
+	for (int i = 1; i < H - 1; i++) {
+		for (int j = 1; j < W - 1; j++) {
+			if (Img[i * W + j] == 0)	//배경화소라면
+			{
+				if (!(Img[(i - 1) * W + j] == 0 &&
+					Img[(i + 1) * W + j] == 0 &&
+					Img[i * W + j - 1] == 0 &&
+					Img[i * W + j + 1] == 0))	//4주변 화소가 모두 배경화소가 아니라면
+					Out[i * W + j] = 255;
+				else
+					Out[i * W + j] = 0;
+			}
+			else
+				Out[i * W + j] = 255;
+		}
+	}
+}
+
+int main()
+{
+	BITMAPFILEHEADER hf; // 14바이트
+	BITMAPINFOHEADER hInfo; // 40바이트
+	RGBQUAD hRGB[256]; // 1024바이트
+
+	FILE* fp;
+	fp = fopen("dilation.bmp", "rb");
+	if (fp == NULL) {
+		printf("File not found!\n");
+		return -1;
+	}
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
+
+	int H = hInfo.biHeight;
+	int W = hInfo.biWidth;
+	int ImgSize = H * W;
+
+	BYTE* Image;
+	BYTE* Output;
+
+	if (hInfo.biBitCount == 24) { // 트루컬러
+		Image = (BYTE*)malloc(ImgSize * 3);
+		Output = (BYTE*)malloc(ImgSize * 3);
+		fread(Image, sizeof(BYTE), ImgSize * 3, fp);
+	}
+	else { // 인덱스(그레이)
+		fread(hRGB, sizeof(RGBQUAD), 256, fp);
+		Image = (BYTE*)malloc(ImgSize);
+		Output = (BYTE*)malloc(ImgSize);
+		fread(Image, sizeof(BYTE), ImgSize, fp);
+	}
+	fclose(fp);
+
+
+	Dilation(Image, Output, W, H);
+	Dilation(Output, Image, W, H);
+	Dilation(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+	Erosion(Image, Output, W, H);
+	Erosion(Output, Image, W, H);
+
+	SaveBMPFile(hf, hInfo, hRGB, W, H, Image, "output_dilation.bmp");
+
+	free(Image);
+	free(Output);
+
+	return 0;
+}
